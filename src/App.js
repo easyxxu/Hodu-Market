7ee8e38cad590cb3a62225de7cd598d1d125b542
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-07-19 11:52:07 +0900

    Design: 글로벌 스타일 작성 - 글로벌 스타일 추가 - 폰트 적용 - reset.css 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Header from "./components/Header";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    font-family: 'Spoqa Han Sans Neo', 'Spoqa Han Sans JP', sans-serif;
+    box-sizing: border-box;
+  }
+  a{
+    font-family: 'Spoqa Han Sans Neo', 'Spoqa Han Sans JP', sans-serif;
+    color: inherit;
+    text-decoration: none;
+  }
+  button{
+    font-family: 'Spoqa Han Sans Neo', 'Spoqa Han Sans JP', sans-serif;
+    border: 0;
+    cursor: pointer;
+  }
+  .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
+  `;
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
   );
 }
 export default App;

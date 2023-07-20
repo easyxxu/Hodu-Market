2dd0abd5cc426b9bc1557c6c9f78bdfb156a661e
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-07-20 12:18:22 +0900

    Design: Home페이지에 Footer 추가 - Home 페이지에 Footer 추가 Related to #2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
-import Header from "../../components/common/Header";
+import Header from "../../components/common/Header/Header";
 import styled from "styled-components";
 import Product from "../../components/Product/Product";
+import Footer from "../../components/common/Footer/Footer";
 
 const HomeContainer = styled.div`
   width: 100%;
@@ -13,6 +14,7 @@ export default function Home() {
       <Header />
       <Banner />
       <Product />
+      <Footer />
     </HomeContainer>
   );
 }

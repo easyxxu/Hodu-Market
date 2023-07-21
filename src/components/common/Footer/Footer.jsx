d9901f1cd4e905a33e9ff598791b5471bd3dbb4c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-07-21 09:07:05 +0900

    Style: 스타일 분리

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import styled from "styled-components";
 import InstaIcon from "../../../assets/icon-insta.svg";
 import FacebookIcon from "../../../assets/icon-fb.svg";
 import YoutubeIcon from "../../../assets/icon-yt.svg";
+import {
+  FooterContainer,
+  FooterStyleContainer,
+  ListContainer,
+  FooterLink,
+  FooterSns,
+  FooterInfo,
+} from "./FooterStyle";
+
 export default function Footer() {
   return (
     <FooterContainer>
@@ -58,51 +66,3 @@ export default function Footer() {
     </FooterContainer>
   );
 }
-const FooterContainer = styled.footer`
-  background-color: #f2f2f2;
-  width: 100%;
-`;
-const FooterStyleContainer = styled.div`
-  max-width: 1200px;
-  padding: 54px 0;
-  margin: 0 auto;
-`;
-const ListContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom: 1px solid #c4c4c4;
-`;
-const FooterLink = styled.ul`
-  display: flex;
-  gap: 32px;
-  margin-bottom: 30px;
-  li {
-    position: relative;
-    &:not(:last-child) {
-      &::after {
-        content: "|";
-        position: absolute;
-        top: 50%;
-        right: -14px;
-        transform: translate(50%, -50%);
-      }
-    }
-  }
-`;
-const FooterSns = styled.ul`
-  display: flex;
-  gap: 12px;
-  margin-bottom: 30px;
-`;
-const FooterInfo = styled.address`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  color: #767676;
-  line-height: 24px;
-  strong {
-    font-weight: 700;
-  }
-`;

9c353105c350a57d127bb2aa981e6de81070eb90
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-05 00:35:13 +0900

    Design: Header 컴포넌트 type에 따라 분리, SellerHeader 추가 - 구매자 or 판매자인지 구분할 수 있도록 type을 추가함 - 판매자 센터 헤더 추가(SellerHeader)

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -4,8 +4,41 @@ import Logo from "../../../assets/Logo-hodu.svg";
 import ShoppingCart from "../../../assets/icon-shopping-cart.svg";
 import MyPage from "../../../assets/icon-user.svg";
 import SearchIcon from "../../../assets/search.svg";
+import ShoppingBag from "../../../assets/icon-shopping-bag.svg";
+import { Button } from "../Button/Button";
 
-export default function Header() {
+const HeaderType = (type) => {
+  if (type === "customer") {
+    return (
+      <Nav>
+        <ShoppingCartLink href="/">
+          <img src={ShoppingCart} alt="shopping-cart" />
+          <p>장바구니</p>
+        </ShoppingCartLink>
+        <LoginLink href="/">
+          <img src={MyPage} alt="my-page" />
+          <p>로그인</p>
+        </LoginLink>
+      </Nav>
+    );
+  } else if (type === "seller") {
+    return (
+      <Nav>
+        <LoginLink href="/">
+          <img src={MyPage} alt="my-page" />
+          <p>마이페이지</p>
+        </LoginLink>
+        <Button
+          width="MS"
+          color="white"
+          img={ShoppingBag}
+          content="판매자센터"
+        />
+      </Nav>
+    );
+  }
+};
+export function Header({ type }) {
   return (
     <HeaderDiv>
       <HeaderContainer>
@@ -16,24 +49,28 @@ export default function Header() {
         </h1>
         <SearchContainer>
           <SearchInput type="text" placeholder="상품을 검색해보세요!" />
-          <SearchBtn></SearchBtn>
+          <SearchBtn aria-label="검색하기 버튼" />
         </SearchContainer>
-        <Nav>
-          <ShoppingCartLink href="/">
-            <img src={ShoppingCart} alt="shopping-cart" />
-            <p>장바구니</p>
-          </ShoppingCartLink>
-          <LoginLink href="/">
-            <img src={MyPage} alt="my-page" />
-            <p>로그인</p>
-          </LoginLink>
-        </Nav>
+        {HeaderType(type)}
       </HeaderContainer>
     </HeaderDiv>
   );
 }
+export function SellerHeader() {
+  return (
+    <HeaderDiv>
+      <SellerHeaderDiv>
+        <h1>
+          <a href="/">
+            <img src={Logo} alt="호두 로고" />
+          </a>
+        </h1>
+        <h2>판매자센터</h2>
+      </SellerHeaderDiv>
+    </HeaderDiv>
+  );
+}
 const HeaderDiv = styled.div`
-  /* padding: 0 350px; */
   box-shadow: 0px 5px 5px -5px rgba(0, 0, 0, 0.1);
   position: relative;
 `;
@@ -53,7 +90,6 @@ const SearchContainer = styled.div`
   position: relative;
   width: 400px;
   min-width: 190px;
-  /* margin-left: 30px; */
 `;
 
 const SearchInput = styled.input`
@@ -81,6 +117,7 @@ const SearchBtn = styled.button`
 const Nav = styled.nav`
   display: flex;
   gap: 26px;
+  align-items: center;
 `;
 
 const ShoppingCartLink = styled.a`
@@ -93,4 +130,22 @@ const LoginLink = styled.a`
   text-align: center;
   color: #767676;
   font-size: 12px;
+`;
+// 판매자센터 헤더
+const SellerHeaderDiv = styled.header`
+  padding: 26px 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  gap: 16px;
+  h2 {
+    font-size: 30px;
+    font-weight: 500;
+  }
+  img {
+    width: 80px;
+    height: 24px;
+  }
 `;

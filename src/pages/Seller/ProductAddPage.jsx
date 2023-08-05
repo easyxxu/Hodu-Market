4f028cfdbc15c5a91e69223bf535d5f773d4e4f0
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-05 16:31:14 +0900

    Design: 상품등록 페이지 구현 - 상품등록 페이지 - 컴포넌트 : ProductAdd.jsx Close #11

---
+++ src/pages/Seller/ProductAddPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import ProductAdd from "../../components/Seller/ProductAdd";
+
+export default function ProductAddPage() {
+  return (
+    <Layout>
+      <ProductAdd />
+    </Layout>
+  );
+}

b3513a55a4a243acbf09b0b1578b492e4e5e9c00
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-05 16:57:20 +0900

    Design: 상품 상세정보 페이지 구현 - ProductDetailPage.jsx 구현 - 컴포넌트 : ProductDetail.jsx Close #5

---
+++ src/pages/Product/ProductDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import ProductDetail from "../../components/Product/ProductDetail";
+
+export default function ProductDetailPage() {
+  return (
+    <Layout type="customer">
+      <ProductDetail />
+    </Layout>
+  );
+}

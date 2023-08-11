ec2242b10617b8009f433a7843b2720b87ddfcef
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-11 23:07:21 +0900

    Feat: ID 중복 검사, 전화번호 버튼 활성화 - ID 중복 검사를 위한 API 통신 - 아이디, 비밀번호, 비밀번호 재확인 결과에 따른 input 창 스타일 변화 추가 - 전화번호 버튼 클릭 시 리스트 활성화 Related to #16

--- src/components/Auth/JoinForm.jsx
+++ src/components/Auth/JoinForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { idDuplicateCheckApi } from "../../apis/signupApi";
 import { Button } from "../common/Button/Button";
 import * as S from "./JoinFormStyle";
 export default function JoinForm() {
@@ -13,22 +14,41 @@ export default function JoinForm() {
   const [pwValidErrorMsg, setPwValidErrorMsg] = useState("");
   const [pwDoubleCheckErrorMsg, setPwDoubleCheckErrorMsg] = useState("");
   const [idValid, setIdValid] = useState(false);
+  const [idDuplicateValid, setIdDuplicateValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
   const [pwDoubleValid, setPwDoubleValid] = useState(false);
+  const [phoneFirst, setPhoneFirst] = useState("");
+  const [phoneListVisible, setPhoneListVisible] = useState(false);
   const handleSubmit = (e) => {};
 
+  const handlePhoneListItemClick = (e) => {
+    setPhoneFirst(e.target.textContent);
+    setPhoneListVisible(false);
+  };
   // onChange 발생
   const handleInputChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
+  const phoneFirstList = ["010", "011", "016", "017", "018", "019"];
+
+  const phoneList = (
+    <ul>
+      {phoneFirstList.map((phonePrefix) => (
+        <li key={phonePrefix} onClick={handlePhoneListItemClick}>
+          {phonePrefix}
+        </li>
+      ))}
+    </ul>
+  );
+
   // onBlur 발생
   const idValidCheck = (e) => {
     const regex = /^[a-zA-Z0-9]{1,20}$/;
     if (!regex.test(e.target.value) && e.target.value.length > 0) {
-      console.log(regex.test(e.target.value));
       setIdValidErrorMsg(
         "ID는 20자 이내의 영어 소문자, 대문자, 숫자만 가능합니다."
       );
+      setIdValid(false);
     } else if (e.target.value.length < 1) {
       setIdValidErrorMsg("필수 정보입니다");
     } else {
@@ -36,21 +56,48 @@ export default function JoinForm() {
       setIdValid(true);
     }
   };
+  // ID 중복 검사 API
+  const idDuplicateCheck = async (id) => {
+    try {
+      const res = await idDuplicateCheckApi(id);
+      if (
+        idValidErrorMsg !==
+          "ID는 20자 이내의 영어 소문자, 대문자, 숫자만 가능합니다." &&
+        res.data.Success === "멋진 아이디네요 :)"
+      ) {
+        setIdValidErrorMsg(res.data.Success);
+        setIdDuplicateValid(true);
+      }
+    } catch (err) {
+      if (err.response.data.FAIL_Message === "이미 사용 중인 아이디입니다.")
+        setIdValidErrorMsg(err.response.data.FAIL_Message);
+      setIdDuplicateValid(false);
+    }
+  };
   const pwValidCheck = (e) => {
     const regex = /^(?=.*[a-z])(?=.*\d)[a-zA-Z\d]{8,}$/;
     if (!regex.test(e.target.value) && e.target.value.length > 0) {
       console.log(e.target.value, regex.test(e.target.value));
-      setPwValidErrorMsg("8자 이상, 영문 대소문자, 숫자를 사용하세요.");
+      setPwValidErrorMsg("8자 이상, 영문 소문자, 숫자를 사용하세요.");
+      setPwValid(false);
+      setPwDoubleValid(false);
     } else if (e.target.value.length < 1) {
       setPwValidErrorMsg("필수 정보입니다.");
+      setPwValid(false);
+      setPwDoubleValid(false);
     } else {
       setPwValidErrorMsg("");
       setPwValid(true);
     }
   };
+
   const pwDoubleCheck = (e) => {
-    if (userInfo.password !== e.target.value) {
+    if (userInfo.password !== e.target.value && userInfo.password.length > 0) {
       setPwDoubleCheckErrorMsg("비밀번호가 일치하지 않습니다.");
+      setPwDoubleValid(false);
+    } else if (pwValidErrorMsg === "필수 정보입니다.") {
+      setPwDoubleCheckErrorMsg("비밀번호가 일치하지 않습니다.");
+      setPwDoubleValid(false);
     } else {
       setPwDoubleCheckErrorMsg("");
       setPwDoubleValid(true);
@@ -66,7 +113,11 @@ export default function JoinForm() {
       <S.Form onSubmit={handleSubmit}>
         <S.JoinFormContainer>
           <label htmlFor="id">아이디</label>
-          <S.IdContainer>
+          <S.IdContainer
+            valid={
+              idValidErrorMsg.length > 0 && idValid === false ? "false" : null
+            }
+          >
             <input
               name="username"
               id="id"
@@ -74,9 +125,19 @@ export default function JoinForm() {
               onChange={handleInputChange}
               onBlur={idValidCheck}
             />
-            <Button content="중복확인" />
+            <Button
+              type="button"
+              content="중복확인"
+              onClick={() => idDuplicateCheck(userInfo.username)}
+            />
           </S.IdContainer>
-          <S.ErrorMsg>{idValidErrorMsg}</S.ErrorMsg>
+          <S.ErrorMsg
+            valid={
+              idValid === true && idDuplicateValid === true ? "success" : null
+            }
+          >
+            {idValidErrorMsg}
+          </S.ErrorMsg>
           <label htmlFor="password">
             비밀번호
             <S.CheckIconStyle
@@ -110,15 +171,14 @@ export default function JoinForm() {
           <fieldset>
             <label htmlFor="phone">휴대폰번호</label>
             <S.PhoneContainer>
-              <button type="button">010</button>
-              <ul>
-                <li>010</li>
-                <li>011</li>
-                <li>016</li>
-                <li>017</li>
-                <li>018</li>
-                <li>019</li>
-              </ul>
+              <S.PhoneFirstBtn
+                type="button"
+                state={phoneListVisible ? "active" : null}
+                onClick={() => setPhoneListVisible(!phoneListVisible)}
+              >
+                {phoneFirst || "010"}
+              </S.PhoneFirstBtn>
+              {phoneListVisible && phoneList}
               <input type="text" />
               <input type="text" />
             </S.PhoneContainer>

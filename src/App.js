029467848111862a5186a1d9b1fac3d8feb8957e
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-23 15:27:04 +0900

    Feat: 라우터, 리코일

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import styled from "styled-components";
 import { GlobalStyle } from "./components/style/GlobalStyle";
-import Login from "./pages/Login/Login";
-
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-`;
+import Routers from "./routes/Routers";
 
 function App() {
   return (
-    <Container>
+    <>
       <GlobalStyle />
-      {/* <Home /> */}
-      <Login />
-    </Container>
+      <Routers />
+    </>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>
+);

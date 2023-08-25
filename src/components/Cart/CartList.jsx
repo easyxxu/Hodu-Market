30812095efe06beef6132c3dde78b7779fc79f5c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-25 17:38:59 +0900

    Fix: li에 key값 추가

--- src/components/Cart/CartList.jsx
+++ src/components/Cart/CartList.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import CartItem from "./CartItem";
 import { useRecoilValue } from "recoil";
 import { cartItemAtom } from "../../atoms/cartAtom";
 export default function CartList() {
   const cartItemList = useRecoilValue(cartItemAtom);
+  const CartList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  `;
   return (
-    <ul>
+    <CartList>
       {cartItemList.map((item) => {
-        return <CartItem item={item} />;
+        return <CartItem item={item} key={item.data.product_id} />;
       })}
-    </ul>
+    </CartList>
   );
 }

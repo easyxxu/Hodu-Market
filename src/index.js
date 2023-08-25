5812dec6f40d3b9084e708a62be8f0d9d657522c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-25 09:47:03 +0900

    Fix: 페이지 이동 시 상단에서부터 시작하도록 수정 - 기존 페이지의 스크롤을 유지한채로 다음 페이지로 넘어가는 문제 - useLocation을 활용해 pathname을 가져오고, useEffect를 통해 pathname이 변할 때 마다 스크롤의 위치를 (0,0)으로 바꿔줌 Closed #33

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <RecoilRoot>
+      <ScrollToTop />
       <App />
     </RecoilRoot>
   </BrowserRouter>

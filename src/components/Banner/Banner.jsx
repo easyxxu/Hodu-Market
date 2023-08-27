9a9a91afbbff85ec36ebacdbaba49d8f556f69e6
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-08-27 13:51:28 +0900

    Refactor: 버튼 접근성 향상

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,11 @@ import { BannerContainer, BannerLeftBtn, BannerRightBtn } from "./BannerStyle";
 export default function Banner() {
   return (
     <BannerContainer>
-      <BannerLeftBtn type="button"></BannerLeftBtn>
-      <BannerRightBtn type="button"></BannerRightBtn>
+      <BannerLeftBtn type="button" aria-label="왼쪽 배너 버튼"></BannerLeftBtn>
+      <BannerRightBtn
+        type="button"
+        aria-label="오른쪽 배너 버튼"
+      ></BannerRightBtn>
     </BannerContainer>
   );
 }

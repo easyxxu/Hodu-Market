af9bbf4f4b1cd3abba2cd32ac7f8cff9cd177a9e
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-05 17:31:00 +0900

    Design: 레이아웃 설정

--- src/pages/Order/OrderPage.jsx
+++ src/pages/Order/OrderPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { MainLayout } from "../../components/Layout/Layout";
 import OrderList from "../../components/Order/OrderList";
 import Payment from "../../components/Order/Payment";
 
 export default function OrderPage() {
+  const userType = localStorage.getItem("user_type");
   return (
-    <div>
+    <MainLayout type={userType}>
       <OrderList />
       <Payment />
-    </div>
+    </MainLayout>
   );
 }

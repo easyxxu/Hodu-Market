745ed83591c9cb1745f611e0bd59049e22b55c72
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-06 15:08:09 +0900

    Style: Payment 스타일 분리 작업

--- src/components/Order/Payment.jsx
+++ src/components/Order/Payment.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import styled from "styled-components";
 import { orderDirect } from "../../apis/orderApi";
-import { Button, ButtonStyle } from "../common/Button/Button";
+import { Button } from "../common/Button/Button";
+import * as S from "./PaymentStyle";
 export default function Payment() {
   const location = useLocation();
   const productData = location.state;
@@ -171,47 +171,47 @@ export default function Payment() {
   console.log(orderForm);
   return (
     <>
-      <Title>배송정보</Title>
+      <S.Title>배송정보</S.Title>
       <hr />
-      <PaymentForm autoComplete="off" onSubmit={handleSubmitOrderForm}>
+      <S.PaymentForm autoComplete="off" onSubmit={handleSubmitOrderForm}>
         <fieldset>
           <legend>주문자 정보</legend>
-          <Label>
+          <S.Label>
             이름
-            <Input
+            <S.Input
               type="text"
               name="receiver"
               onChange={handleOrderPersonName}
             />
-          </Label>
-          <Label>
+          </S.Label>
+          <S.Label>
             휴대폰
-            <PhoneInputContainer>
-              <Input
+            <S.PhoneInputContainer>
+              <S.Input
                 type="text"
                 name="first"
                 maxLength="3"
                 onBlur={handleOrderPhoneNum}
               />
-              <Input
+              <S.Input
                 type="text"
                 name="second"
                 maxLength="4"
                 onBlur={handleOrderPhoneNum}
               />
-              <Input
+              <S.Input
                 type="text"
                 name="third"
                 maxLength="4"
                 onBlur={handleOrderPhoneNum}
               />
-            </PhoneInputContainer>
-          </Label>
-          <Label>
+            </S.PhoneInputContainer>
+          </S.Label>
+          <S.Label>
             이메일
-            <Input type="email" name="email" onChange={handleOrderEmail} />
-          </Label>
-          <InfoSaveBtn
+            <S.Input type="email" name="email" onChange={handleOrderEmail} />
+          </S.Label>
+          <S.InfoSaveBtn
             bgcolor="disabled"
             color="white"
             width="MS"
@@ -219,61 +219,61 @@ export default function Payment() {
             onClick={handleSaveInfoBtn}
           >
             주문자 정보와 동일
-          </InfoSaveBtn>
+          </S.InfoSaveBtn>
         </fieldset>
         <fieldset>
           <legend>배송지 정보</legend>
-          <Label>
+          <S.Label>
             수령인
-            <Input type="text" name="receiver" onChange={handleInputChange} />
-          </Label>
-          <Label>
+            <S.Input type="text" name="receiver" onChange={handleInputChange} />
+          </S.Label>
+          <S.Label>
             휴대폰
-            <PhoneInputContainer>
-              <Input
+            <S.PhoneInputContainer>
+              <S.Input
                 type="text"
                 name="first"
                 maxLength="3"
                 onChange={handleReceiverPhoneNum}
               />
-              <Input
+              <S.Input
                 type="text"
                 name="second"
                 maxLength="4"
                 onChange={handleReceiverPhoneNum}
               />
-              <Input
+              <S.Input
                 type="text"
                 name="third"
                 maxLength="4"
                 onChange={handleReceiverPhoneNum}
               />
-            </PhoneInputContainer>
-          </Label>
-          <ShippingLabel>
+            </S.PhoneInputContainer>
+          </S.Label>
+          <S.ShippingLabel>
             배송주소
-            <ShippingInfo>
-              <Input type="text" name="우편번호" onChange={handleAddress} />
-              <Input type="text" name="기본주소" onChange={handleAddress} />
-              <Input type="text" name="상세주소" onChange={handleAddress} />
-            </ShippingInfo>
-          </ShippingLabel>
-          <ShippingMsgLabel>
+            <S.ShippingInfo>
+              <S.Input type="text" name="우편번호" onChange={handleAddress} />
+              <S.Input type="text" name="기본주소" onChange={handleAddress} />
+              <S.Input type="text" name="상세주소" onChange={handleAddress} />
+            </S.ShippingInfo>
+          </S.ShippingLabel>
+          <S.ShippingMsgLabel>
             배송 메시지
-            <ShippingMsgInput
+            <S.ShippingMsgInput
               type="text"
               name="address_message"
               onChange={handleInputChange}
             />
-          </ShippingMsgLabel>
-          <ShippingBtn width="154px" color="white" type="button">
+          </S.ShippingMsgLabel>
+          <S.ShippingBtn width="154px" color="white" type="button">
             우편번호 조회
-          </ShippingBtn>
+          </S.ShippingBtn>
         </fieldset>
-        <TotalPaymentContainer>
-          <PaymentFieldset>
+        <S.TotalPaymentContainer>
+          <S.PaymentFieldset>
             <legend>결제수단</legend>
-            <PaymentContainer>
+            <S.PaymentContainer>
               <label>
                 <input
                   type="radio"
@@ -319,35 +319,35 @@ export default function Payment() {
                 />
                 카카오페이
               </label>
-            </PaymentContainer>
-          </PaymentFieldset>
+            </S.PaymentContainer>
+          </S.PaymentFieldset>
           <div>
-            <FinalPaymentTitle>최종결제 정보</FinalPaymentTitle>
-            <FinalPaymentContainer>
-              <FinalPaymentDetail>
+            <S.FinalPaymentTitle>최종결제 정보</S.FinalPaymentTitle>
+            <S.FinalPaymentContainer>
+              <S.FinalPaymentDetail>
                 <p>- 상품금액</p>
                 <p>
                   <strong>{productData.productPrice}</strong>원
                 </p>
-              </FinalPaymentDetail>
-              <FinalPaymentDetail>
+              </S.FinalPaymentDetail>
+              <S.FinalPaymentDetail>
                 <p>- 할인금액</p>
                 <p>
                   <strong>0</strong>원
                 </p>
-              </FinalPaymentDetail>
-              <FinalPaymentDetail>
+              </S.FinalPaymentDetail>
+              <S.FinalPaymentDetail>
                 <p>- 배송비</p>
                 <p>
                   <strong>{productData.productShippingFee}</strong>원
                 </p>
-              </FinalPaymentDetail>
+              </S.FinalPaymentDetail>
               <hr />
-              <FinalPayment>
+              <S.FinalPayment>
                 <p>- 결제금액</p>
                 <strong>{total} 원</strong>
-              </FinalPayment>
-              <FinalAgreementContainer>
+              </S.FinalPayment>
+              <S.FinalAgreementContainer>
                 <label>
                   <input
                     type="checkbox"
@@ -365,205 +365,11 @@ export default function Payment() {
                   content="결제하기"
                   disabled={!handleSubmitBtn()}
                 />
-              </FinalAgreementContainer>
-            </FinalPaymentContainer>
+              </S.FinalAgreementContainer>
+            </S.FinalPaymentContainer>
           </div>
-        </TotalPaymentContainer>
-      </PaymentForm>
+        </S.TotalPaymentContainer>
+      </S.PaymentForm>
     </>
   );
 }
-
-const Title = styled.h3`
-  font-size: 24px;
-  font-weight: 500;
-  margin-top: 156px;
-`;
-const PaymentForm = styled.form`
-  margin: 40px 0 70px;
-  position: relative;
-  fieldset:first-child {
-    margin-bottom: 30px;
-    position: relative;
-  }
-  fieldset:nth-child(2) {
-    margin-bottom: 70px;
-    position: relative;
-  }
-  legend {
-    width: 1280px;
-    font-size: 18px;
-    font-weight: 500;
-    border-bottom: 2px solid var(--content-color-light);
-    padding-bottom: 8px;
-  }
-`;
-const InfoSaveBtn = styled(ButtonStyle)`
-  margin-top: 15px;
-`;
-const Label = styled.label`
-  width: 504px;
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-size: 16px;
-  &::after {
-    content: "";
-    position: absolute;
-    width: 1280px;
-    height: 1px;
-    bottom: 0;
-    background-color: var(--content-color-light);
-  }
-`;
-const Input = styled.input`
-  width: 334px;
-  height: 40px;
-  margin: 8px 0;
-  border: 1px solid var(--content-color-light);
-  font-size: 16px;
-`;
-const PhoneInputContainer = styled.div`
-  display: flex;
-  gap: 27px;
-  z-index: 1;
-  input {
-    position: relative;
-    &:first-child {
-      width: 80px;
-    }
-    &:not(:first-child) {
-      width: 100px;
-    }
-  }
-  &::before {
-    z-index: 2;
-    content: "-";
-    position: absolute;
-    top: 25%;
-    left: 51%;
-    transform: translate(50%, 50%);
-    font-size: 16px;
-  }
-  &::after {
-    z-index: 2;
-    content: "-";
-    position: absolute;
-    top: 25%;
-    right: 22.5%;
-    transform: translate(50%, 50%);
-    font-size: 16px;
-  }
-`;
-const ShippingLabel = styled(Label)`
-  width: 970px;
-  &::after {
-    top: 168px;
-  }
-`;
-const ShippingInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  input:first-child {
-    width: 170px;
-  }
-  input:nth-child(2),
-  input:nth-child(3) {
-    width: 800px;
-  }
-`;
-const ShippingBtn = styled(ButtonStyle)`
-  position: absolute;
-  top: 35.7%;
-  left: 27%;
-`;
-const ShippingMsgLabel = styled(Label)`
-  width: 970px;
-`;
-const ShippingMsgInput = styled(Input)`
-  width: 800px;
-`;
-const PaymentContainer = styled.div`
-  display: flex;
-  gap: 20px;
-  margin-top: 18px;
-  padding-bottom: 20px;
-  border-bottom: 2px solid var(--content-color-light);
-`;
-const TotalPaymentContainer = styled.div`
-  display: flex;
-`;
-const PaymentFieldset = styled.fieldset`
-  width: 760px;
-  margin-right: 40px;
-  legend {
-    width: 760px;
-  }
-  input {
-    margin-right: 10px;
-  }
-`;
-const FinalPaymentTitle = styled.h4`
-  font-size: 24px;
-  font-weight: 500;
-  margin-bottom: 18px;
-`;
-const FinalPaymentContainer = styled.div`
-  border: 2px solid var(--point-color);
-  width: 480px;
-  padding: 34px 30px;
-  border-radius: 10px;
-  z-index: 4;
-  position: relative;
-  &::after {
-    content: "";
-    position: absolute;
-    z-index: 1;
-    background-color: #f2f2f2;
-    width: 476px;
-    height: 182px;
-    bottom: 0;
-    left: 0;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-  }
-`;
-const FinalPaymentDetail = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 15px;
-  p {
-    font-size: 16px;
-  }
-  p:last-child {
-    color: var(--content-color-dark);
-  }
-  strong {
-    font-weight: 700;
-    font-size: 18px;
-    margin-right: 4px;
-    color: black;
-  }
-`;
-const FinalPayment = styled(FinalPaymentDetail)`
-  margin: 29px 0 55px;
-  align-items: center;
-  strong {
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 700;
-    color: var(--price-point-color);
-  }
-`;
-const FinalAgreementContainer = styled.div`
-  z-index: 2;
-  position: relative;
-  input {
-    margin-right: 10px;
-  }
-  button {
-    margin-top: 30px;
-    margin-left: 92px;
-  }
-`;

0249ae19a43b6b8f043cac71d9ff08d567f5c798
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-11 17:22:31 +0900

    Fix: 이전의 상품 정보가 나오는 문제 - 홈에서 여러 상품 페이지를 확인하면 이전에 확인한 상품 정보가 나오는 문제 - productId가 전역상태로 관리되어 이전 값이 보이는 것 같음 - productId가 전역상태로 관리될 필요성이 없어 삭제 Closed #65

--- src/pages/Product/ProductDetailPage.jsx
+++ src/pages/Product/ProductDetailPage.jsx
@@ -2,47 +2,30 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useRecoilState } from "recoil";
 import { loadProductDetail } from "../../apis/productApi";
-import { productIdAtom } from "../../atoms/productAtom";
 import { MainLayout } from "../../components/Layout/Layout";
 import ProductDetail from "../../components/Product/ProductDetail";
 
 export default function ProductDetailPage() {
-  const { id } = useParams();
-  const [productId, setProductId] = useRecoilState(productIdAtom);
+  const { productId } = useParams();
   const [productInfo, setProductInfo] = useState({});
   const userType = localStorage.getItem("user_type");
 
-  // productId 업데이트 시 useEffect를 통해 getProductDetail 호출
   useEffect(() => {
-    setProductId(id);
-  }, [id]);
-
-  const getProductDetail = async (productId) => {
-    try {
-      const res = await loadProductDetail(productId);
-      setProductInfo(res.data);
-    } catch (err) {
-      console.error("getProductDetail Error: ", err);
-    }
-  };
-
-  useEffect(() => {
+    const getProductDetail = async (productId) => {
+      try {
+        const res = await loadProductDetail(productId);
+        setProductInfo(res.data);
+      } catch (err) {
+        console.error("상품 상세정보 Error: ", err.response.data);
+      }
+    };
     getProductDetail(productId);
   }, [productId]);
+
   return (
     <MainLayout type={userType}>
-      <ProductDetail
-        productInfo={productInfo}
-        // storeName={productInfo.store_name}
-        // productName={productInfo.product_name}
-        // productImg={productInfo.image}
-        // productPrice={productInfo.price}
-        // productShippingMethod={productInfo.shipping_method}
-        // productShippingFee={productInfo.shipping_fee}
-        // productDescription={productInfo.product_info}
-      />
+      <ProductDetail productInfo={productInfo} />
     </MainLayout>
   );
 }

c7aecc95c34a4af26a8c12b715ad7fafc947c02b
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-11 16:51:04 +0900

    Feat: 주문 번호 클릭 시 주문 상세페이지 화면에 출력 - 주문 번호 클릭 시 주문 상세페이지 화면에 출력 - 테이블 디자인 수정 Related to #64

--- src/components/MyPage/OrderList.jsx
+++ src/components/MyPage/OrderList.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { fetchOrderList } from "../../apis/orderApi";
 import { styled } from "styled-components";
+import OrderDetail from "./OrderDetail";
 
 export default function OrderList() {
   const [orderListData, setOrderListData] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(null);
   // 날짜 형식 변경 함수
   const formatDateTime = (dateTime) => {
     return new Date(dateTime).toLocaleString("ko-KR");
   };
-  const onClickOrderDetail = () => {};
+  const onClickOrderDetail = (order) => {
+    setSelectedOrder(order);
+  };
   useEffect(() => {
     const getOrderList = async () => {
       try {
@@ -24,55 +28,73 @@ export default function OrderList() {
   console.log(orderListData);
   return (
     <OrderTableContainer>
-      <OrderTable>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>주문번호</th>
-            <th>주문상품</th>
-            <th>배송상태</th>
-            <th>구매일자</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orderListData.map((order, idx) => {
-            const formattedDate = formatDateTime(order.created_at);
-            return (
-              <tr key={idx}>
-                <td>{idx + 1}</td>
-                <td>{order.order_number}</td>
-                <td>{order.order_items}</td>
-                <td>
-                  {order.delivery_status === "COMPLETE_PAYMENT"
-                    ? "결제완료"
-                    : null}
-                </td>
-                <td>{formattedDate}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </OrderTable>
+      {!selectedOrder ? (
+        <OrderTable>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>주문번호</th>
+              <th>배송상태</th>
+              <th>구매일자</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderListData.map((order, idx) => {
+              const formattedDate = formatDateTime(order.created_at);
+              return (
+                <tr key={idx} onClick={() => onClickOrderDetail(order)}>
+                  <td>{idx + 1}</td>
+                  <td>{order.order_number}</td>
+                  <td>
+                    {order.delivery_status === "COMPLETE_PAYMENT"
+                      ? "결제완료"
+                      : null}
+                  </td>
+                  <td>{formattedDate}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </OrderTable>
+      ) : (
+        <OrderDetail
+          order={selectedOrder}
+          onClose={() => setSelectedOrder(null)}
+        />
+      )}
     </OrderTableContainer>
   );
 }
 const OrderTableContainer = styled.div`
-  background-color: var(--content-color-light);
+  /* background-color: var(--content-color-light); */
   height: 100%;
 `;
 const OrderTable = styled.table`
-  border: 1px solid var(--content-color-dark);
+  border-bottom: 3px solid #f2f2f2;
+  border-radius: 10px;
+  border-collapse: seperate;
   width: 100%;
   background-color: #fff;
   th {
-    font-size: 18px;
-    padding: 10px;
+    font-size: 24px;
+    padding: 15px;
+    background-color: #f2f2f2;
+    &:first-child {
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
+    &:last-child {
+      border-top-right-radius: 10px;
+      border-bottom-right-radius: 10px;
+    }
   }
   tr {
-    border: 1px solid var(--content-color-light);
+    &:not(:last-child) {
+      border-bottom: 1px solid var(--content-color-light);
+    }
   }
   td {
     text-align: center;
-    padding: 10px 0;
+    padding: 15px 0;
   }
 `;

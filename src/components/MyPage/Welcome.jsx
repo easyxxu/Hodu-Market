71c14fcb21c6bc85f3d34b86c634d0dad14491b8
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-17 20:55:14 +0900

    Design: 판매자의 경우 마이페이지에서 로그아웃만 하도록 변경 Related to #73

--- src/components/MyPage/Welcome.jsx
+++ src/components/MyPage/Welcome.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { styled } from "styled-components";
 
-export default function Welcome({ name }) {
+export default function Welcome() {
   return (
     <Container>
-      <p>
-        <strong>{name}님</strong> 안녕하세요.
-      </p>
+      <p>안녕하세요.</p>
       <p>
         저희 호두마켓은 자신만의 마켓을 만들어 상품을 판매할 수 있고 <br />
         또한, 구매자로서 상품을 구매 할 수 있는 오픈마켓 서비스입니다.
@@ -19,10 +17,6 @@ const Container = styled.div`
     &:first-child {
       font-size: 32px;
       font-weight: 700;
-      /* color: var(--content-color-dark); */
-      strong {
-        box-shadow: inset 0px -11px 0 var(--point-color);
-      }
     }
     &:last-child {
       font-size: 28px;

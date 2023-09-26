65be3576293a5310dec0289676d1bc0de445c522
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-09-26 22:02:55 +0900

    Design: 브레이크포인트 변수화

---
+++ src/components/style/media.ts
@@ -0,0 +1,16 @@
+type SizeType = "Medium" | "Small";
+
+const sizes: Record<SizeType, number> = {
+  // Large: 1200,
+  Medium: 1200,
+  Small: 639,
+};
+
+export const media = (Object.keys(sizes) as Array<keyof typeof sizes>).reduce(
+  (acc, label) => {
+    acc[label] = (style: string) =>
+      `@media (max-width: ${sizes[label]}px) { ${style} }`;
+    return acc;
+  },
+  {} as { [index: string]: Function }
+);

22bca959799601dfac8ddc211a007854a481e6f6
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-10-08 23:03:27 +0900

    Fix: API 통신 시 권한 없음 에러 해결 - interceptors 사용하여 문제 해결 Closed to #84

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,24 +1,32 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const BASE_URL = "https://openmarket.weniv.co.kr";
-// 기본 인스턴스
-export const axiosInstance = axios.create({
+
+const baseConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
-});
+};
 
-export const privateInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    Authorization: `JWT ${localStorage.getItem("token")}`,
-    "Content-Type": "application/json",
-  },
-});
+const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `JWT ${token}`;
+  }
+  return config;
+};
+
+// 기본 인스턴스
+export const axiosInstance = axios.create(baseConfig);
+
+// 인증 요청 인스턴스
+export const privateInstance = axios.create(baseConfig);
+privateInstance.interceptors.request.use(onRequest as any);
 
 export const imgPrivateInstance = axios.create({
-  baseURL: BASE_URL,
+  ...baseConfig,
   headers: {
-    Authorization: `JWT ${localStorage.getItem("token")}`,
     "Content-Type": "multipart/form-data",
   },
 });
+imgPrivateInstance.interceptors.request.use(onRequest as any);

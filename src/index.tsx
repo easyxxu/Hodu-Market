c8848b1db0ca0c53ae74de408ec7be7485db826f
easyxxu <107910342+easyxxu@users.noreply.github.com>
2024-03-21 00:04:19 +0900

    Feat: react-snap 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
@@ -7,13 +8,28 @@ import ScrollToTop from "./components/common/ScrollToTop";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-root.render(
-  <BrowserRouter>
-    <RecoilRoot>
-      <ScrollToTop />
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </RecoilRoot>
-  </BrowserRouter>
-);
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <BrowserRouter>
+      <RecoilRoot>
+        <ScrollToTop />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
+} else {
+  root.render(
+    <BrowserRouter>
+      <RecoilRoot>
+        <ScrollToTop />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </RecoilRoot>
+    </BrowserRouter>
+  );
+}
